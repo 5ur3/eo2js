3c19875d29066f9a4ea9d879f3dc7bae900d4e98
Artem Murashko <Artem.Murashko@kaspersky.com>
2023-05-17 02:49:03 +0300

    add if to booleans

--- eolib/booleans.js
+++ eolib/booleans.js
@@ -1,6 +1,11 @@
 const bool = (value) => ({
     $_value: () => value,
     $_datarize: () => value.toString(),
+
+    if: (obj1, obj2) => ({
+        $_value: () => value ? obj1() : obj2(),
+        $_datarize: () => value ? obj1().$_datarize() : obj2().$_datarize()
+    }),
 })
 
 const boolTrue = () => bool(true)
