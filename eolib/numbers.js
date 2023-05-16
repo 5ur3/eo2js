934bfd67566d0fc9f40f70283558680aba7b885b
Artem Murashko <Artem.Murashko@kaspersky.com>
2023-05-17 01:47:50 +0300

    add numbers operations

--- eolib/numbers.js
+++ eolib/numbers.js
@@ -1,11 +1,14 @@
 const num = (value) => ({
   $_value: () => value,
   $_datarize: () => value.toString(),
-  add: (obj) => num(value + obj().$_value())
+  add: (obj) => num(value + obj().$_value()),
+  sub: (obj) => num(value - obj().$_value()),
+  mul: (obj) => num(value * obj().$_value()),
+  div: (obj) => num(value / obj().$_value())
 })
 
-const int = num
 const float = num
+const int = num
 
 module.exports = {
   num,
