0e60f3fc546e9a66f0d16a4a9a477eb515476abf
Artem Murashko <Artem.Murashko@kaspersky.com>
2023-05-16 22:43:39 +0300

    add value parsing

--- src/eo2js/parseXmlObject.ts
+++ src/eo2js/parseXmlObject.ts
@@ -1,4 +1,6 @@
 import { ParsedObject, ParsedObjectType } from './ParsedObject'
+import {eo2js} from "./index";
+import {parseString} from "xml2js";
 
 export const parseXmlObject = (object: any): ParsedObject => {
   const baseObject = {
@@ -9,7 +11,7 @@ export const parseXmlObject = (object: any): ParsedObject => {
     return {
       type: ParsedObjectType.closed,
       base: object['$'].base,
-      value: object['_'],
+      value: parseValue(object['$'].base, object['_']),
       ...baseObject
     }
   } else if (object['$'].abstract === '') {
@@ -24,5 +26,31 @@ export const parseXmlObject = (object: any): ParsedObject => {
       isList: object['$'].vararg === '' ? true : undefined,
       ...baseObject
     }
+  }
+}
+
+const parseValue = (base: string, value: string) => {
+  switch (base) {
+    case 'org.eolang.float':
+      const hexValues = value.split(' ');
+      const uint8Array = new Uint8Array(hexValues.map(hex => parseInt(hex, 16)));
+      const dataView = new DataView(uint8Array.buffer);
+
+      return dataView.getFloat64(0).toString();
+
+    case 'org.eolang.int':
+      return parseInt(value.replace(/ /g, ''), 16).toString()
+
+    case 'org.eolang.string':
+      return value.split(' ').map(hex => String.fromCharCode(parseInt(hex, 16))).join('');
+
+    case 'org.eolang.true':
+      return "true"
+
+    case 'org.eolang.false':
+      return "false"
+
+    default:
+      return ""
   }
 }
