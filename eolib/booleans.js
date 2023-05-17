62fb6ab835ffcd68d180c03b5c90a7d669ffd09f
Artem Murashko <Artem.Murashko@kaspersky.com>
2023-05-17 03:45:10 +0300

    add and + or to bool

--- eolib/booleans.js
+++ eolib/booleans.js
@@ -6,7 +6,10 @@ const bool = (value) => ({
         $_value: () => value ? obj1() : obj2(),
         $_datarize: () => value ? obj1().$_datarize() : obj2().$_datarize()
     }),
+
     not: () => bool(!value),
+    and: (obj) => bool(value && obj().$_value()),
+    or: (obj) => bool(value || obj().$_value()),
 })
 
 const boolTrue = () => bool(true)
