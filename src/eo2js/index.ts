5f9deda24b4dd17fec0c8a0d09a594bf1c898b77
5ur3 <an.sandimirov@gmail.com>
2023-05-17 03:26:54 +0300

    Remove debug

--- src/eo2js/index.ts
+++ src/eo2js/index.ts
@@ -7,7 +7,6 @@ export const eo2js = async (xml: string) => {
   const program = (await parseXml(xml)).program
   const objects = program.objects[0].o
   const parsedObjects = objects.map(o => buildParsedObject(o))
-  console.log(JSON.stringify(objects))
   const jsObjects = parsedObjects.map(o => buildJsObject(o))
   return makeJsScript(jsObjects)
 }
